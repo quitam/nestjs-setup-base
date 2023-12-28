721fca6ce1e86aab190ee5413a00809533423d09
quitam <tamphamasd@gmail.com>
2023-12-28 13:42:49 +0700

    update nestjs setting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api/v1');
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
     .setDescription('NestJS API description')
@@ -12,7 +11,7 @@ async function bootstrap() {
     .addTag('nestjs')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1', app, document);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 }
 bootstrap();
