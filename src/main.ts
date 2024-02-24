6e721608266cefcf894beb480967a6840fd1df16
quitam <tamphamasd@gmail.com>
2024-02-24 23:48:56 +0700

    update: oke

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 5000);
-  console.log('Server is running on http://localhost:5000');
+  console.log('Server is running on http://localhost:okeddddd');
 }
 bootstrap();
