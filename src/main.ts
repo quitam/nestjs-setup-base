68585299945ded328d892b02ffec76168f4ab9e2
Pham Qui Tam <90175373+quitam@users.noreply.github.com>
2024-02-25 00:17:15 +0700

    log ok

--- src/main.ts
+++ src/main.ts
@@ -14,5 +14,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 5000);
   console.log('Server is running on http://14.225.211.133:5000');
+  console.log('OK');
 }
 bootstrap();
